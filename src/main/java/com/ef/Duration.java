4c2a8fe976b48e9cbd774e7e306ddfc12313d032
srfunksensei <sr.funk.sensei@gmail.com>
2021-03-05 11:14:03 +0100

    cleaned up Duration enum

--- src/main/java/com/ef/Duration.java
+++ src/main/java/com/ef/Duration.java
@@ -10,18 +10,14 @@ public enum Duration {
 
     HOURLY("HOURLY"), DAILY("DAILY");
 
-    private String label;
+    private final String label;
 
     Duration(final String label) {
         this.label = label.toUpperCase();
     }
 
-    Duration() {
-    }
-
     public static Duration findByLabel(String byLabel) {
         for (final Duration type : Duration.values()) {
-
             if (type.label.equalsIgnoreCase(byLabel.trim())) {
                 return type;
             }
