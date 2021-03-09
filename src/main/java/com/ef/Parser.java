7c3b323c4f2b1e6851a35e716518d44b51de4d43
srfunksensei <sr.funk.sensei@gmail.com>
2021-03-09 10:02:30 +0100

    cleaned up Parser (main class)

--- src/main/java/com/ef/Parser.java
+++ src/main/java/com/ef/Parser.java
@@ -1,22 +1,21 @@
 package com.ef;
 
+import com.ef.entity.HibernateUtil;
+import com.ef.entity.WebAccessLog;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-
-import com.ef.entity.HibernateUtil;
-import com.ef.entity.WebAccessLog;
-
 public class Parser {
 
-    public static final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd.HH:mm:ss", Locale.US);
+    public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd.HH:mm:ss", Locale.US);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         if (args.length > 0) {
             Date startDate = null;
             Duration duration = null;
@@ -28,43 +27,42 @@ public class Parser {
 
                 switch (params[0]) {
 
-                case "--startDate":
-                    try {
-                        startDate = format.parse(params[1]);
-                    } catch (ParseException e) {
-                        e.printStackTrace();
-                    }
-                    break;
+                    case "--startDate":
+                        startDate = DATE_FORMAT.parse(params[1]);
+                        break;
 
-                case "--duration":
-                    if (Duration.findByLabel(params[1].toUpperCase()) == null) {
-                        throw new IllegalArgumentException("Argument" + params[0] + " must be an hourly or daily");
-                    }
-                    duration = Duration.findByLabel(params[1].toUpperCase());
-                    break;
+                    case "--duration":
+                        final Duration durationParsed = Duration.findByLabel(params[1].toUpperCase());
+                        if (durationParsed == null) {
+                            throw new IllegalArgumentException("Argument" + params[0] + " must be an hourly or daily");
+                        }
+                        duration = durationParsed;
+                        break;
 
-                case "--threshold":
-                    if (Integer.valueOf(params[1]) <= 0) {
-                        throw new IllegalArgumentException("Argument" + params[0] + " must be an integer");
-                    }
-                    threshold = Long.valueOf(params[1]);
-                    break;
+                    case "--threshold":
+                        if (Long.parseLong(params[1]) <= 0) {
+                            throw new IllegalArgumentException("Argument" + params[0] + " must be positive number");
+                        }
+                        threshold = Long.parseLong(params[1]);
+                        break;
 
-                case "--accesslog":
-                    filePath = params[1];
-                    break;
+                    case "--accesslog":
+                        filePath = params[1];
+                        break;
                 }
             }
 
             if (startDate == null || duration == null) {
                 throw new IllegalArgumentException("Not all required params passed");
             }
-            
-            if (duration.equals(Duration.HOURLY)) {
-                threshold = 200;
-            } else if (duration.equals(Duration.DAILY)) {
-                threshold = 500;
-            } 
+
+            if (threshold <= 0) {
+                if (Duration.HOURLY.equals(duration)) {
+                    threshold = 200;
+                } else if (Duration.DAILY.equals(duration)) {
+                    threshold = 500;
+                }
+            }
 
             final Session session = HibernateUtil.getSessionFactory().openSession();
             Transaction transaction = null;
@@ -72,7 +70,7 @@ public class Parser {
                 transaction = session.beginTransaction();
 
                 final List<WebAccessLog> records = WebAccessLogParser.parseFile(filePath);
-                records.forEach(e -> session.saveOrUpdate(e));
+                records.forEach(session::saveOrUpdate);
 
                 final BlockedIpFilter filter = new BlockedIpFilter(records);
                 filter.getBlockedIpAddresses(startDate, duration, threshold).forEach(e -> {
@@ -82,7 +80,9 @@ public class Parser {
 
                 transaction.commit();
             } catch (Exception ex) {
-                transaction.rollback();
+                if (transaction != null) {
+                    transaction.rollback();
+                }
                 ex.printStackTrace();
             } finally {
                 session.close();
